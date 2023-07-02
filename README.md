e99251d710efcfed0b20f8e0c1e6b64506459115
codi huston <codi.huston540@gmail.com>
2023-07-02 09:10:16 -0500

    Add export traces to Jaeger

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # open-telemetry-getting-started
+
+# Run Jaeger
+
+From the [Jaeger docs](https://www.jaegertracing.io/docs/1.46/getting-started/):
+
+```
+docker run -d --name jaeger \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -e COLLECTOR_OTLP_ENABLED=true \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 14250:14250 \
+  -p 14268:14268 \
+  -p 14269:14269 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:1.46
+```
+
+# Run Application
+
+```
+go run .
+```
+
+# View Traces
+
+Visit http://localhost:16686/search and search for traces from the `fib` service.
--- main.go
+++ main.go
@@ -5,10 +5,79 @@ import (
 	"log"
 	"os"
 	"os/signal"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
+
+// name is the Tracer name used to identify this instrumentation library.
+const name = "fib"
+const jaegerUrl = "http://localhost:14268/api/traces"
+
+// newExporter returns a console exporter.
+// func newExporter(w io.Writer) (trace.SpanExporter, error) {
+// 	return stdouttrace.New(
+// 		stdouttrace.WithWriter(w),
+// 		// Use human-readable output.
+// 		stdouttrace.WithPrettyPrint(),
+// 		// Do not print timestamps for the demo.
+// 		stdouttrace.WithoutTimestamps(),
+// 	)
+// }
+
+func newExporter(url string) (trace.SpanExporter, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	return exp, nil
+}
+
+// newResource returns a resource describing this application.
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("fib"),
+			semconv.ServiceVersion("v0.1.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
+}
 
 func main() {
 	l := log.New(os.Stdout, "", 0)
+
+	// Write telemetry data to a file.
+	f, err := os.Create("traces.txt")
+	if err != nil {
+		l.Fatal(err)
+	}
+	defer f.Close()
+
+	exp, err := newExporter(jaegerUrl)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(newResource()),
+	)
+
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal(err)
+		}
+	}()
+	otel.SetTracerProvider(tp)
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
